e03b7cd377a7bc0026d0c6c75f920ea2b7fa039b
Aditya Raghuwanshi <adi.version1@gmail.com>
2018-03-02 16:23:58 -0800

    Setup Javascript Testing with Karma/Jasmine

---
+++ spec/javascript/karma.conf.js
@@ -0,0 +1,11 @@
+module.exports = function(config) {
+  config.set({
+    frameworks: ['jasmine'],
+    files: [ '**/*.spec.js' ],
+    preprocessors: {
+      '**/*.spec.js': [ 'webpack' ] },
+      webpack: require('../../config/webpack/karma.js'),
+      browsers: ['PhantomJS']
+    }
+  );
+}
