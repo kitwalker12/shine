a1b1daeb1296bfd264199c54287a8ba7af44c655
Aditya Raghuwanshi <adi.version1@gmail.com>
2018-03-02 17:22:10 -0800

    separate out html code into template file for component

--- config/webpack/environment.js
+++ config/webpack/environment.js
@@ -1,5 +1,7 @@
 const { environment } = require('@rails/webpacker')
 const typescript =  require('./loaders/typescript')
+const html =  require('./loaders/html')
 
 environment.loaders.append('typescript', typescript)
+environment.loaders.append('html', html)
 module.exports = environment
