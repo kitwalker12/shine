a930f05c7f8b8410bc2dc8a325ca3bee8589d313
Aditya Raghuwanshi <adi.version1@gmail.com>
2018-03-02 17:50:10 -0800

    Use Angular Routing

--- app/controllers/customers_controller.rb
+++ app/controllers/customers_controller.rb
@@ -1,6 +1,11 @@
 class CustomersController < ApplicationController
   PAGE_SIZE = 10
 
+  def ng
+    @base_url = "/customers/ng"
+    render :index
+  end
+
   def index
     @page = (params[:page] || 0).to_i
 
@@ -15,7 +20,9 @@ class CustomersController < ApplicationController
     end
 
     respond_to do |format|
-      format.html {}
+      format.html {
+        redirect_to customers_ng_path
+      }
       format.json {
         render json: { customers: @customers }
       }
---
+++ app/javascript/CustomerDetailsComponent/index.ts
@@ -0,0 +1,13 @@
+import { Component } from "@angular/core";
+import template from "./template.html";
+
+var CustomerDetailsComponent = Component({
+  selector: "shine-customer-details",
+  template: template
+}).Class({
+  constructor: [
+    function() {}
+  ]
+});
+
+export { CustomerDetailsComponent }
