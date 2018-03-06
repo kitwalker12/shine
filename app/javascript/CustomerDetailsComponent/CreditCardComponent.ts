69d88ca0713003a2dc51a5f64cf381bd444edc04
Aditya Raghuwanshi <adi.version1@gmail.com>
2018-03-05 16:45:29 -0800

    Fetch Credit Card info from second source

--- app/javascript/CustomerDetailsComponent/CreditCardComponent.ts
+++ app/javascript/CustomerDetailsComponent/CreditCardComponent.ts
@@ -1,14 +1,47 @@
 import { Component } from "@angular/core";
+import { Http } from "@angular/http";
 import   template    from "./CreditCardComponent.html";
 
 var CreditCardComponent = Component({
   selector: "shine-credit-card",
-  template: template
+  template: template,
+  inputs: [
+    "cardholder_id"
+  ]
 }).Class({
   constructor: [
-    function() {
+    Http,
+    function(http) {
+      this.http = http;
+      this.cardholder_id = null;
     }
-  ]
+  ],
+
+  ngOnChanges: function(changes) {
+    if (changes.cardholder_id) {
+      if (changes.cardholder_id.currentValue) {
+        this.cardholder_id = changes.cardholder_id.currentValue;
+        this.fetchCreditCardInfo();
+      }
+      else {
+        this.cardholder_id    = null;
+        this.credit_card_info = null;
+      }
+    }
+  },
+
+  fetchCreditCardInfo: function() {
+    var self = this;
+    self.http.get("/credit_card_info/" + self.cardholder_id).
+      subscribe(
+        function(response) {
+          self.credit_card_info = response.json().credit_card_info;
+        },
+        function(response) {
+          console.log(response);
+        }
+    );
+  }
 });
 
 export { CreditCardComponent };
